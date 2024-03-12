9811d3aeb67f4da4ad6c17e54f7913e34b05d6c6
PedroHSBernardes <133461209+PedroHenriqueSBer@users.noreply.github.com>
2024-03-12 17:33:14 -0300

    Initial Commit

---
+++ Atividade1/Controllers/ConvidadoController.cs
@@ -0,0 +1,77 @@
+﻿using Atividade1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atividade1.Controllers
+{
+    public class ConvidadoController : Controller
+    {
+        public static IList<Convidado> convidados = new List<Convidado>()
+        {
+            new Convidado() { Id = 1,Nome="João Lucas Lourenço", Telefone="35997275693",EMail="jao.lucas@gmail.com",comparecimento=false}
+        };
+
+
+        public IActionResult Index()
+        {
+            return View(convidados);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Convidado convidado)
+        {
+            convidado.Id = convidados.Select(conv => conv.Id).Max() + 1;
+            convidados.Add(convidado);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Details(long id)
+        {
+            return View(convidados.Where(conv => conv.Id == id).First());
+        }
+
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Convidado convidado)
+        {
+            convidados.Remove(convidados.Where(conv => conv.Id == convidado.Id).First());
+            convidados.Add(convidado);
+            return RedirectToAction("Index");
+        }
+
+        
+
+        public IActionResult Delete(int id)
+        {
+            return View(convidados.Where(conv => conv.Id == id).First());
+        }
+        [HttpPost]
+        public IActionResult Delete(Convidado convidado)
+        {
+            convidados.Remove(convidados.Where(conv => conv.Id == convidado.Id).First());
+            return RedirectToAction("Index");
+        }
+
+
+
+        public IActionResult SortByConfirmation()
+        {
+            return View(convidados.Where(conv => conv.comparecimento == true));
+        }
+
+        public IActionResult SortByNotConfirmation()
+        {
+            return View(convidados.Where(conv => conv.comparecimento == false));
+        }
+    }
+}
---
+++ Atividade1/Models/Convidado.cs
@@ -0,0 +1,12 @@
+﻿namespace Atividade1.Models
+{
+    public class Convidado
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Telefone { get; set; }
+        public bool comparecimento { get; set; }
+        public string comparecimentoString { get => comparecimento ? "Sim" : "Não"; }
+    }
+}
